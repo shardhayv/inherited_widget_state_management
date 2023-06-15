f316946232680dcd6803d89204c0fdaefd6ee07a
shardhayv <shardhayvatshyayan7@gmail.com>
2023-06-15 18:52:06 +0545

    api provider class for inherited widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'State Management',
       home: StateManagement(),
     ),
   );
+}
+
+class ApiProvider extends InheritedWidget {
+  final Api api;
+  final String uuid;
+
+  ApiProvider({
+    Key? key,
+    required this.api,
+    required Widget child,
+  })  : uuid = const Uuid().v4(),
+        super(key: key, child: child);
 }
 
 class StateManagement extends StatefulWidget {
@@ -35,6 +48,22 @@ class _StateManagementState extends State<StateManagement> {
           color: Colors.grey,
         ),
       ),
+    );
+  }
+}
+
+class Api {
+  String? dateAndTime;
+
+  Future<String> getDateAndTime() {
+    return Future.delayed(
+      const Duration(seconds: 1),
+      () => DateTime.now().toIso8601String(),
+    ).then(
+      (value) {
+        dateAndTime = value;
+        return value;
+      },
     );
   }
 }
